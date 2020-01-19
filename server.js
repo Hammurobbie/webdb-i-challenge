c7dddf4d3e695f3c5929f7efb0b2400ac0eb9a19
Robbie Porter <rthomporter@gmail.com>
2020-01-19 01:44:30 -0600

    initial commit

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
-const express = require('express');
+const express = require("express");
 
-const db = require('./data/dbConfig.js');
+const db = require("./data/dbConfig.js");
 
 const server = express();
 
 server.use(express.json());
+
+server.get("/", (req, res) => {
+  db("accounts");
+});
 
 module.exports = server;
