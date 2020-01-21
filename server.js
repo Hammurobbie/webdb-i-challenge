a5da71503f3571a5b96dc2f78545bc9461b0b90f
Robbie Porter <rthomporter@gmail.com>
2020-01-21 02:33:19 -0600

    MVP

--- server.js
+++ server.js
@@ -6,8 +6,83 @@ const server = express();
 
 server.use(express.json());
 
+server.post("/", (req, res) => {
+  db("accounts")
+    .insert(req.body)
+    .then(acc => {
+      console.log(acc);
+      res.status(200).json(acc);
+    })
+    .catch(() =>
+      res
+        .status(400)
+        .json({ error: "There was a problem retrieving the account." })
+    );
+});
+
 server.get("/", (req, res) => {
-  db("accounts");
+  db("accounts")
+    .then(acc => {
+      console.log(acc);
+      res.status(200).json(acc);
+    })
+    .catch(() =>
+      res
+        .status(400)
+        .json({ error: "There was a problem retrieving the accounts." })
+    );
+});
+
+server.get("/:id", (req, res) => {
+  const { id } = req.params;
+
+  db("accounts")
+    .where({ id })
+    .then(acc => {
+      console.log(acc);
+      res.status(200).json(acc);
+    })
+    .catch(() =>
+      res
+        .status(400)
+        .json({ error: "There was a problem retrieving the account." })
+    );
+});
+
+server.put("/:id", (req, res) => {
+  const { id } = req.params;
+
+  db("accounts")
+    .where({ id })
+    .update(req.body)
+    .then(acc => {
+      console.log(acc);
+      res.status(200).json(acc);
+    })
+    .catch(() =>
+      res
+        .status(400)
+        .json({ error: "There was a problem updating the account." })
+    );
+});
+
+server.delete("/:id", (req, res) => {
+  const { id } = req.params;
+
+  db("accounts")
+    .where({ id })
+    .del()
+    .then(acc => {
+      console.log(acc);
+      res
+        .status(200)
+        .json({ message: "The account was successfully deleted." });
+    })
+    .catch(() =>
+      res
+        .status(400)
+        .json({ error: "There was a problem deleting the account." })
+    );
 });
 
 module.exports = server;
